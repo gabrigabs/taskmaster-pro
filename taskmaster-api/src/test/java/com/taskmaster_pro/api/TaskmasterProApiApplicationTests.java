0a994753a2700bb26f4eedba6ca534906c46c6ac
Gabriel <gabrielbrodrigues1@gmail.com>
2025-03-21 21:32:09 -0300

    feat: initialize backend app

---
+++ taskmaster-api/src/test/java/com/taskmaster_pro/api/TaskmasterProApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskmaster_pro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmasterProApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
